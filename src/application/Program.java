4b1b068d973ee3a363da2e92351b926ab13d827f
DESKTOP-OMU538G\Gabriel <gabriel10moines@gmail.com>
2023-08-26 00:40:33 -0300

    primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite uma palavra: ");
+        String palavra = sc.nextLine();
+        sc.close();
+        
+        if (isPalindrome(palavra)) {
+            System.out.println("É um palíndromo!");
+        } else {
+            System.out.println("Não é um palíndromo.");
+        }
+    }
+    
+    public static boolean isPalindrome(String palavra) {
+        palavra = palavra.toLowerCase(); // Converter para minúsculas para comparação sem diferenciação de maiúsculas/minúsculas
+        int esquerda = 0;
+        int direita = palavra.length() - 1;
+        
+        while (esquerda < direita) {
+            if (palavra.charAt(esquerda) != palavra.charAt(direita)) {
+                return false;
+            }
+            esquerda++;
+            direita--;
+        }
+        return true;
+    }
+}
